4585857e490564a47de6c62246c675c5e508d747
Tsandesh <tandesh69@gmail.com>
2024-01-13 12:33:12 +0545

    where is cors

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 app.use(express.json());
@@ -8,7 +9,7 @@ app.use(express.json());
 const corsOptions = {
   origin: ["https://nike-store-mern.vercel.app", "http://localhost:5173"],
 };
-
+const cors = require("cors");
 app.use(cors(corsOptions));
 
 // checkout api
